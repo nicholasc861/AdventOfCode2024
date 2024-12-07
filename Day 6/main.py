0ae6f6151f06564e9a41bafd4631f633fd54d5b0
Nicholas Chung-Hun <nicholas.chunghun@gmail.com>
2024-12-06 19:54:33 -0500

    finish day 6, part 1

--- Day 6/main.py
+++ Day 6/main.py
@@ -1,4 +1,5 @@
 import os
+from collections import deque
 
 # Standard boilerplate to read input file
 dirname = os.path.dirname(__file__)
@@ -15,6 +16,37 @@ if __name__ == "__main__":
     for idx, group in enumerate(groups):
         if ("^" in group):
             starting_pos = (idx, group.index("^"))
+            
+    dirs = deque([(-1, 0), (0, 1), (1, 0), (0, -1)])
+    current_dir = dirs.popleft()
+    num_visited = 1 # include starting position
+    
+    obstacle_encountered = {}
+            
+    queue = [(starting_pos[0] + current_dir[0], starting_pos[1] + current_dir[1])]
+    while len(queue):
+        (next_x, next_y) = queue.pop()
+        
+        if next_x < len(groups) and next_y < len(groups[0]) and next_x >= 0 and next_y >= 0:
+            square = groups[next_x][next_y]
+    
+            if square != "#":
+                # only count distinct squares
+                if square == ".":
+                    num_visited += 1
+                    
+                queue.append((next_x + current_dir[0], next_y + current_dir[1]))
+                
+                string = list(groups[next_x])
+                string[next_y] = "X"
+                groups[next_x] = ''.join(string)
+            else:
+                curr_x = next_x - current_dir[0]
+                curr_y = next_y - current_dir[1]
+                
+                dirs.append(current_dir)
+                current_dir = dirs.popleft()
+                
+                queue.append((curr_x + current_dir[0], curr_y + current_dir[1]))
 
-
-    print(starting_pos)
+    print(num_visited)
